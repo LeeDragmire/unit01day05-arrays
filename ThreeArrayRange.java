a3f1e9121e758271f5c60d387e5f525f71fed89c
colton <adamscolton64@gmail.com>
2023-01-18 11:27:08 -0600

    Arrays | ThreeArrayRange Complete

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,28 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        try {
+            int[] a = {Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])};
+            if (a[0]>a[1]&&a[0]>a[2]) {
+                if (a[1]>a[2]) {
+                    System.out.println(a[0] - a[2]);
+                } else {
+                    System.out.println(a[0] - a[1]);
+                }
+            } else if (a[1]>a[0]&&a[1]>a[2]) {
+                if (a[0]>a[2]) {
+                    System.out.println(a[1]-a[2]);
+                } else {
+                    System.out.println(a[1]-a[0]);
+                }
+            } else {
+                if (a[0]>a[1]) {
+                    System.out.println(a[2]-a[1]);
+                } else {
+                    System.out.println(a[2]-a[0]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("try again");
+        }
     }
 }
