a720b7cdfd99a9b71158fd8e05c8e4a28a40dfb9
colton <adamscolton64@gmail.com>
2023-01-18 11:38:52 -0600

    Arrays | ImmutableSort Complete

--- ImmutableSort.java
+++ ImmutableSort.java
@@ -1,6 +1,36 @@
 public class ImmutableSort {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
-        
+        int[] a = {Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])};
+        int[] b = {0,0,0};
+        if (a[0]<a[1]&&a[0]<a[2]) {
+            b[0] = a[0];
+            if (a[1]<a[2]) {
+                b[1] = a[1];
+                b[2] = a[2];
+            } else {
+                b[1] = a[2];
+                b[2] = a[1];
+            }
+        } else if (a[1]<a[0]&&a[1]<a[2]) {
+            b[0] = a[1];
+            if (a[0]<a[2]) {
+                b[1] = a[0];
+                b[2] = a[2];
+            } else {
+                b[1] = a[2];
+                b[2] = a[0];
+            }
+        } else {
+            b[0] = a[2];
+            if (a[0]<a[1]) {
+                b[1] = a[0];
+                b[2] = a[1];
+            } else {
+                b[1] = a[1];
+                b[2] = a[0];
+            }
+        }
+        System.out.println(a[0] + " " + a[1] + " " + a[2]);
+        System.out.println(b[0] + " " + b[1] + " " + b[2]);
     }
 }
